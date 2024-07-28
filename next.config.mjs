72867542a386114bbdcfa1f65526ce3b4e5d1451
Celso Franciscano <celsofranciscano24@gmail.com>
2024-07-28 18:55:00 -0400

    add enviroment in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+      URL_API: process.env.URL_API
+    },
+  };
+  
+  export default nextConfig;
+  
